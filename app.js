f75b142aab576268bd959601ed61d0e615dd2dad
Daniel Napierski <danielpnapierski@gmail.com>
2018-12-17 16:45:05 -0500

    Added first Civic routing and civic-sip-api client

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var civic = require('./routes/civic');
 
 var tediousExpress = require('express4-tedious');
 var app = express();
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/civic', civic);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
